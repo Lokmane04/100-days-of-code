efa0b45b4dda141dc53b62a64c89cb33d50110a7
ReD <baslilokmane04@gmail.com>
2023-11-23 08:43:42 +0100

    day 1
    the first python programme , ban name generator

---
+++ day 1/main.py
@@ -0,0 +1,7 @@
+# The Name Generator -- my first Python project
+print("Welcome the Band name generator : ")
+print("what's the name of the city you grew up in ?: ")
+city = input()
+print("what's the name of your pet ? : ")
+pet = input()
+print("Your band name could be : "+city+' '+pet)
