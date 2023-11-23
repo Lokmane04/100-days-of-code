17ebf2b0fea6cca8abefe44d161ccbee4a7b6ca2
ReD <baslilokmane04@gmail.com>
2023-11-23 14:21:57 +0100

    day 4  :  exercice 1
    selecting random people to pay for dinner

---
+++ day 4/exercice1.py
@@ -0,0 +1,4 @@
+import random
+names = input('Type in the names , separate them with comma the n a space (eg: eimane, habib, lokmane) : ').split(', ')
+print('\n')
+print(names[random.randint(1,len(names)-1)],'is going to pay for dinner 🥲 🥲')
