b59ae423f611900d0841f115053e6931ae02e665
ReD <baslilokmane04@gmail.com>
2023-11-24 08:25:32 +0100

    creating the FizzBuzz game

--- day5/average_height.py
+++ day5/average_height.py
@@ -1,7 +1,8 @@
 print("----------welcome to the height generator---------")
 students_heights = input("Type in the student's heights in cm (eg : 154 163 184 156) : ").split(' ')
-print(students_heights)
 average_height:float =0
+count = 0
 for height in students_heights:
   average_height += int(height)
-print(average_height/len(students_heights))
+  count+=1
+print(average_height/count)
