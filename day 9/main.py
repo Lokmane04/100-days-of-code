2235e90cf549e295c4890c56c72ee45ad1db8939
ReD <baslilokmane04@gmail.com>
2023-11-27 17:00:38 +0100

    improving the UX

--- day 9/main.py
+++ day 9/main.py
@@ -14,23 +14,27 @@ def max_bid(secret_auction):
 
 secret_auction= {}
 
-name = input('your name : ?')
-bid = int(input('your bid'))
+
+name =input('What is your name ? :')
+bid = int(input("What's your bid ? : "))
+
 secret_auction[name] = bid
+
+
 confirm=True
 while confirm==True:
   other_bidders = input('want to continue ? : ')
   if other_bidders == 'yes':
-    name =input('your name : ?')
-    bid = int(input('your bid'))
+    name =input('What is your name ? :')
+    bid = int(input("What's your bid ? : $"))
     secret_auction[name] = bid
   elif other_bidders =='no':
     confirm=False
   else:
-    print('please type yes or no')
+    print("Are there any other bidders ? :(type 'yes' or 'no )  .\n"  )
     confirm = False
 
 
 
 max = max_bid(secret_auction)
-print(f'the highest is : {max[0]} for "{max[1]}"')
+print(f'the highest is : ${max[0]} for "{max[1]}"')
