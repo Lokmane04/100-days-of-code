1d56b041b2e597b3e6b20bccccc91fe173028cae
ReD <baslilokmane04@gmail.com>
2023-11-29 00:11:56 +0100

    adding medium difficulty level to the game

--- day 12/guess_number.py
+++ day 12/guess_number.py
@@ -1,14 +1,17 @@
 import random
+from ascii_art import welcomig_logo
 #print the welcome message
-print("the number is between 1 and 100")
+print('Welcome to the Number Guessing Game .')
+print("I'm thinking of a number between 1 and 100 .")
+
 # picking random number between 1 and 100
 number = random.randint(1,100)
 attempts =15
 #choose a difficulty
 
-difficulty_level=input("type 'e' or 'h' ")
+difficulty_level=input("Choose a difficulty ? type 'e' for easy , 'h' for hard and 'm' for medium : (the default is easy)\n")
 # 'easy'
-if difficulty_level == 'e':
+if difficulty_level == 'm':
   attempts = 10
 # 'hard'
 elif difficulty_level == 'h': 
@@ -22,14 +25,18 @@ end_of_game =False
 while attempts>0 and end_of_game==False:
 
   #telling the user how many attempts are left
-  print(f"You have {attempts} attempts left")
+  if attempts ==1 :
+
+    print(f"You have 1 attempt left 😬 😬")
+  else:
+    print(f"You have {attempts} attempts left")
 
   #asking the user to guess the number
-  guess = int(input("guess the number : "))
+  guess = int(input("Make a guess : "))
 
   #checking if the user guessed correctly
   if guess == number:
-    print("You win")
+    print("You won !! 🥳 🥳")
     end_of_game = True
   #telling the user if they guessed correctly or not if they did print you win and exit the loop
   elif guess <number:
@@ -40,5 +47,5 @@ while attempts>0 and end_of_game==False:
     print("Too High")
     # if (attempts == 0) print you lost
   if attempts == 0:
-      print("You lost")
+      print("You lost !! 🥲 🥲")
 
