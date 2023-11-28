07279854314be09e1cf107fdee31deffa2374f76
ReD <baslilokmane04@gmail.com>
2023-11-28 23:36:46 +0100

    planning the guess the number programme

---
+++ day 12/guess_number.py
@@ -0,0 +1,28 @@
+#print the welcome message
+print("the number is between 1 and 100")
+
+#choose a difficulty
+
+
+# 'easy'
+
+
+
+# 'hard'
+
+
+
+#inside a while loop 5 for easy , and 10 for hard
+
+
+  #telling the user how many attempts are left
+
+
+  #asking the user to guess the number
+
+  #checking if the user guessed correctly
+
+  #telling the user if they guessed correctly or not if they did print you win and exit the loop
+
+  # if (attempts == 0) print you lost
+
