9c228cd5c440cf651fcb46386824aa372cd96ba1
ReD <baslilokmane04@gmail.com>
2023-11-30 23:40:46 +0100

    working with the make_coffee function , it checks whether the resources are enough or not

--- day 15/main.py
+++ day 15/main.py
@@ -1,5 +1,11 @@
 from data import MENU, resources
 
+# global variables
+MONEY = 0
+CURRENT_RESOURCES = resources.copy()
+CURRENT_RESOURCES["money"] = MONEY
+print(CURRENT_RESOURCES)
+
 
 def check_resources() -> object:
     """This function checks whether resources are enough to make a drink"""
@@ -24,28 +30,43 @@ def affordable(total, response):
     return False
 
 
-def game():
-    response = input('What would you like? (espresso/latte/cappuccino):')
-    if response == 'off':
-        return 0
-    elif response == 'report':
-        print(resources)
-    elif response == 'latte' or response == 'espresso' or response == 'cappuccino':
-        insufficient_resource = check_resources()
-        if insufficient_resource:
-            print(f"sorry there is not enough {insufficient_resource}")
-        else:
-            # quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+def make_coffee(coffee_type, resources):
+    for key in resources:
+        resources[key] -= MENU[coffee_type][key]
+        if resources[key] < 0:
+            print(f"Sorry there is not enough {key} ")
+    return resources
 
-            print("please insert coins :")
-            quarters = int(input("how many quarters you want to insert ? (type 0 if there is none) : "))
-            dimes = int(input("how many dimes you want to insert ? : "))
-            nickles = int(input("how many nickles you want to insert ? : "))
-            pennies = int(input("how many pennies you want to insert ? : "))
-            total = coins_inserted(quarters, dimes, nickles, pennies)
-            if not affordable(total, response):
-                print("Sorry that's not enough money. Money refunded.")
-                return 0
+
+def game():
+    global MONEY, CURRENT_RESOURCES
+    exit_loop = False
+    while not exit_loop:
+        response = input('What would you like? (espresso/latte/cappuccino):')
+        if response == 'off':
+            exit_loop = True
+            return 0
+        elif response == 'report':
+            print(resources)
+        elif response == 'latte' or response == 'espresso' or response == 'cappuccino':
+            insufficient_resource = check_resources()
+            if insufficient_resource:
+                print(f"sorry there is not enough {insufficient_resource}")
+            else:
+                # quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+
+                print("please insert coins :")
+                print(type(response))
+                quarters = int(input("how many quarters you want to insert ? (type 0 if there is none) : "))
+                dimes = int(input("how many dimes you want to insert ? : "))
+                nickles = int(input("how many nickles you want to insert ? : "))
+                pennies = int(input("how many pennies you want to insert ? : "))
+                total = coins_inserted(quarters, dimes, nickles, pennies)
+                if not affordable(total, response):
+                    print("Sorry that's not enough money. Money refunded.")
+                    return 0
+                CURRENT_RESOURCES = make_coffee(response, CURRENT_RESOURCES)
+                # return strings object of the resources left and store them in a variable
 
 
 game()
