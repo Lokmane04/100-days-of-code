c58ab16dd4e0af248c4a6c08baa7bcb6aa7fa7b4
ReD <baslilokmane04@gmail.com>
2023-12-01 22:49:50 +0100

    finishing the quiz game

--- day 16/main.py
+++ day 16/main.py
@@ -6,7 +6,7 @@ coffee_machine = CoffeeMaker()
 money_machine = MoneyMachine()
 menu = Menu()
 EXIT = False
-RESPONSE=''
+RESPONSE = ''
 
 
 def main():
@@ -16,17 +16,13 @@ def main():
         RESPONSE = input(f"What would you like? : ({menu.get_items()}) ")
         if RESPONSE == 'report':
             coffee_machine.report()
+            money_machine.report()
         elif RESPONSE == 'off':
             EXIT = True
         else:
             drink = menu.find_drink(RESPONSE)
-            coffee_machine.is_resource_sufficient(drink)
-            money_machine.make_payment(drink.cost)
-            coffee_machine.make_coffee(drink)
+            if coffee_machine.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_machine.make_coffee(drink)
 
 
 main()
-
-
-
-
