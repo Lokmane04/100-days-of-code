a619133945399c4c9368d75c52bf65f2c0f65560
ReD <baslilokmane04@gmail.com>
2023-12-02 01:05:29 +0100

    drawing a square with the Turtle (tool for gui with python)

---
+++ day 18/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle,Screen
+timmy = Turtle()
+timmy.shape("triangle")
+timmy.color("blue")
+for i in range(4):
+    timmy.forward(100)
+    timmy.left(90)
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
