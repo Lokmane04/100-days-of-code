c92034431524942f0fa1966253621a7c5779a880
ReD <baslilokmane04@gmail.com>
2023-12-02 23:42:23 +0100

    a small project made with Turtle that draws shapes from 3 sides to 9

--- day 18/main.py
+++ day 18/main.py
@@ -1,14 +1,18 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
+
 timmy = Turtle()
 timmy.shape("triangle")
 timmy.color("blue")
-for i in range(4):
-    timmy.forward(100)
-    timmy.left(90)
 
 
+def numberToShape(sides):
+    for i in range(sides):
+        timmy.forward(100)
+        timmy.left(360 / sides)
 
 
+for sides in range(3, 10):
+    numberToShape(sides)
 
 screen = Screen()
 screen.exitonclick()
