f4a9f20e75b5e0d239f18b81ca672964c1ec4bc5
ReD <baslilokmane04@gmail.com>
2023-12-03 11:18:53 +0100

    Added clear_screen function to Turtle graphics

--- day 19/etch_a_sketch.py
+++ day 19/etch_a_sketch.py
@@ -4,32 +4,38 @@ tim = Turtle()
 
 
 def move_forward():
-    tim.setheading(0)
-    tim.forward(100)
+    tim.forward(10)
 
 
 def move_backward():
-    tim.setheading(180)
-    tim.forward(100)
+    tim.forward(10)
 
 
-def move_counter_clockwise():
-    tim.setheading(90)
-    tim.forward(100)
+def turn_left():
+    tim.setheading(tim.heading()+10)
+    tim.forward(10)
 
 
-def move_clockwise():
-    tim.setheading(270)
-    tim.forward(100)
+def turn_right():
+    tim.setheading(tim.heading()-10)
+    tim.forward(10)
+
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 
 screen = Screen()
 
 screen.listen()
 
-screen.onkey(key='w',fun=move_forward)
-screen.onkey(key='s',fun=move_backward)
-screen.onkey(key='a',fun=move_counter_clockwise)
-screen.onkey(key='d', fun=move_clockwise)
+screen.onkey(key='w', fun=move_forward)
+screen.onkey(key='s', fun=move_backward)
+screen.onkey(key='a', fun=turn_left)
+screen.onkey(key='d', fun=turn_right)
+screen.onkey(key='c', fun=clear_screen)
 
 screen.exitonclick()
