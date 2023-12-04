08d93e15b97821cda0f86c99b8eee1505c7bca40
ReD <baslilokmane04@gmail.com>
2023-12-04 20:46:17 +0100

    creating the food class

---
+++ day 20-21/food.py
@@ -0,0 +1,15 @@
+import random
+from turtle import Turtle
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color("yellow")
+        self.speed(0)
+        x_axis= random.randint(-280, 280)
+        y_axis= random.randint(-280, 280)
+        self.goto(x=x_axis, y=y_axis)
--- day 20-21/main.py
+++ day 20-21/main.py
@@ -7,7 +7,7 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
-
+# placing the food
 snake = Snake()
 screen.listen()
 screen.onkey(snake.up, "Up")
