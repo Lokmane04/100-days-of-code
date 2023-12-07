52fd1c905efc98712da44da025189a9a72e3c326
ReD <baslilokmane04@gmail.com>
2023-12-07 18:46:45 +0100

    finishing the mail merge project

--- day 24/main.py
+++ day 24/main.py
@@ -1,4 +1,3 @@
-# TODO: Create a letter using starting_letter.txt
 PLACE_HOLDER = "[name]"
 with open("./Input/Names/invited_names.txt", mode="r") as file:
     names = file.readlines()
@@ -10,5 +9,5 @@ with open("./Input/Letters/starting_letter.txt") as letter_file:
         stripped_name = name.strip()
         new_letter = letter_content.replace(PLACE_HOLDER,  stripped_name)
         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
-            completed_letter.write(stripped_name)
+            completed_letter.write(new_letter)
 
