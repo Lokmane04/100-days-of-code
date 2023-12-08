cacec4732b813dcb9a781422c6f5ba43f736ba10
ReD <baslilokmane04@gmail.com>
2023-12-08 22:45:35 +0100

    feature : finishing the us states project

--- day 25/us_state_game/main.py
+++ day 25/us_state_game/main.py
@@ -1,3 +1,4 @@
+import time
 import turtle
 import pandas
 
@@ -21,15 +22,28 @@ screen.addshape(IMAGE)
 turtle.shape(IMAGE)
 
 # game loop
-
+states_not_guessed=[]
 game_over = False
-correct_answers = 0
+correct_answers = []
 while not game_over:
-    answer = turtle.textinput(title=f"{correct_answers}/50", prompt="Guess other states name:")
+    if len(correct_answers) > 50:
+        print_states.home()
+        print_states.write(f"Congrats , You Won !! 🥳 ", align="center", font=("Arial", 45, "normal"))
+        time.sleep(3)
+        break
+
+    answer = turtle.textinput(title=f"{len(correct_answers)}/50", prompt="Guess other states name:")
+    if answer.lower() == 'exit':
+        break
     for index in range(0, len(STATES) - 1):
         if answer.lower() == STATES[index].lower():
-            correct_answers += 1
+            correct_answers.append(STATES[index])
             print_states.goto(x=x_cor[index], y=y_cor[index])
             print_states.write(f"{STATES[index]}")
 
-turtle.mainloop()
+for state in STATES:
+    if state not in correct_answers:
+        states_not_guessed.append(state)
+df = pandas.DataFrame(states_not_guessed)
+df.to_csv("states_to_learn.csv")
+# you won't find any states to learn because i know them all xd
