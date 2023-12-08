ee9422434d4a476286ce39d886b84fcc5c5e9d6b
ReD <baslilokmane04@gmail.com>
2023-12-08 14:48:52 +0100

    creating a scv file using pandas after extracting the informations from another file

--- day 25/main.py
+++ day 25/main.py
@@ -1,12 +1,20 @@
-import csv
 import pandas
-with open("./002 weather-data.csv") as data_file:
-    weather_data = csv.reader(data_file)
-    temperature =[]
-    for row in weather_data:
-        if row[1]== 'temp':
-            temperature.append(row[1])
-        else:
-            temperature.append(int(row[1]))
 
-print(temperature)
+df = pandas.read_csv("Squirrel_Data.csv")
+
+data_dict = {
+    "Fur Color": ['Gray', 'Cinnamon', 'Black'],
+    "Count": [0, 0, 0]
+}
+primary_fur_color = df["Primary Fur Color"]
+fur_color = data_dict["Fur Color"]
+squirrels_count = data_dict["Count"]
+
+squirrels_count[0] = len(df[primary_fur_color == fur_color[0]])
+squirrels_count[1] = len(df[primary_fur_color == fur_color[1]])
+squirrels_count[2] = len(df[primary_fur_color == fur_color[2]])
+
+print(squirrels_count)
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("squirrel_count.csv")
