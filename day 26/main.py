e3db327dc251feda791c03e6180d3b3baac04f4b
ReD <baslilokmane04@gmail.com>
2023-12-09 23:19:07 +0100

    creating the nato word project

--- day 26/main.py
+++ day 26/main.py
@@ -17,11 +17,27 @@
 #     #Access row.student or row.score
 #     pass
 
+
 # Keyword Method with iterrows()
+
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 # 1. Create a dictionary in this format:
+
 # {"A": "Alfa", "B": "Bravo"}
 
 # 2. Create a list of the phonetic code words from a word that the user inputs.
 
+
+import pandas
+
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+new_dictionary = {row.letter: row.code for (index, row) in df.iterrows()}
+
+word_input = input("Enter a word : ").upper()
+
+
+modified_word = [new_dictionary[letter] for letter in word_input]
+
+print(modified_word)
