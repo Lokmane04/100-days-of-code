1d2750c2fee1ba74357b232e6ac2902c4d5d5c45
ReD <baslilokmane04@gmail.com>
2023-12-10 00:35:40 +0100

    working with the grid , place and pack for the Layout

--- day 27/main.py
+++ day 27/main.py
@@ -1,18 +1,27 @@
 from tkinter import *
 
+
 def clicked():
-    text['text'] = "i got clicked"
+    text.config(text=f"{input_field.get()}")
+
 
 window = Tk()
 
-window.minsize(width=600,height=400)
+window.minsize(width=500, height=300)
 
 title = window.title("First GUI program")
 
-
 text = Label(text='Some text .')
-text.pack()
-button = Button(text="Click me",command=clicked)
-button.pack()
+text.grid(column=0, row=0)
+
+input_field = Entry(width=15)
+
+input_field.grid(column=3,row=0)
+
+button = Button(text="Click me", command=clicked)
+button.grid(column=2,row=1)
+
+new_button = Button(text="new button", command=clicked)
+new_button.grid(column=4,row=4)
 
 window.mainloop()
