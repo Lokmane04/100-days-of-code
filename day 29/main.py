b1ff7169dc9fcfcc0ffb041bff8f652812c896d2
ReD <baslilokmane04@gmail.com>
2023-12-12 06:04:09 +0100

    feat:save password in a separate file locally , reset the entries to indicate the user

--- day 29/main.py
+++ day 29/main.py
@@ -3,9 +3,20 @@ from tkinter import *
 window = Tk()
 window.title("Password manager")
 window.config(pady=50, padx=50)
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save_password():
+    with (open("passwords.txt", mode='a') as file):
+        file_text = f"\n\nwebsite : {website_entry.get()}\nusername/email : {username_entry.get()}\npassword: {password_entry.get()}\n\n\n________________"
+        file.write(file_text)
+        website_entry.delete(0, END)
+        username_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,13 +38,14 @@ username_label.grid(column=0, row=2)
 password_label = Label(text="Password : ")
 password_label.grid(column=0, row=3)
 
-
 # Entries
 website_entry = Entry(width=55)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 username_entry = Entry(width=55)
 username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.insert(0, "red@gmail.com")
 
 password_entry = Entry(width=55)
 password_entry.grid(column=1, row=3, columnspan=2)
@@ -43,7 +55,7 @@ password_entry.grid(column=1, row=3, columnspan=2)
 generate_button = Button(text="Generate password", width=15)
 generate_button.grid(column=2, row=4, columnspan=1)
 
-add_button = Button(text="Add to library")
+add_button = Button(text="Add to library", command=save_password)
 add_button.grid(column=1, row=4, columnspan=1)
 
 window.mainloop()
