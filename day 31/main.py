602f9d5b3181cf93a2ac14e9984ae496113e20e8
ReD <baslilokmane04@gmail.com>
2023-12-19 23:55:39 +0100

    bug: fixing the window flip bug

--- day 31/main.py
+++ day 31/main.py
@@ -17,12 +17,13 @@ else:
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
-    window.after(3000, func=flip_card)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
@@ -43,6 +44,7 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
