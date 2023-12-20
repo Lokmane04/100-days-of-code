7cb9e02db3999d3dc095bfd01808c3706058ccac
ReD <baslilokmane04@gmail.com>
2023-12-21 00:02:19 +0100

    finishing the project

--- day 32/main.py
+++ day 32/main.py
@@ -1,13 +1,27 @@
 import smtplib
+from datetime import datetime as dt
+import pandas
+import random
 
-my_email = 'baslilokmane04@gmail.com'
-app_password = 'dhfeseigwtnhaava'
-reciever = 'lokmanbas999@gmail.com'
-message = "Subject:Quote by Marcus Aurelius\n\nWhen you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love..."
-connection = smtplib.SMTP("smtp.gmail.com")
+today = (dt.now().month, dt.now().day)
 
-connection.starttls()
+# you should change the email and the password
+MY_EMAIL = 'baslilokmane04@gmail.com'
+PASSWORD = 'dhfeseigwtnhaava'
 
-connection.login(user=my_email, password=app_password)
-connection.sendmail(from_addr=my_email, to_addrs=reciever, msg=message)
-connection.close()
+df = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(row.month, row.day): row for (i, row) in df.iterrows()}
+
+letter_num = random.randint(1, 3)
+if today in birthdays_dict:
+    reciever = birthdays_dict[today]
+
+    with open(f"letter_templates/letter_{letter_num}.txt") as letter_file:
+        letter = letter_file.readlines()
+        letter[0] = letter[0].replace("[NAME]", f"{reciever["name"]}")
+        letter = ''.join(letter)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject:Birthday motivation\n\n{letter}")
