4e351b4ce6f19773211f435d8a1fb66261abfa5f
ReD <baslilokmane04@gmail.com>
2023-12-21 20:45:02 +0100

    building the kanye project

--- day 33/kanye-quotes-project/main.py
+++ day 33/kanye-quotes-project/main.py
@@ -6,7 +6,8 @@ import requests
 
 def fetch_quote():
     response = requests.get(url="https://api.kanye.rest")
-
+    # raising exception
+    response.raise_for_status()
     data = response.json()
     quote = data["quote"]
     return quote
@@ -22,9 +23,13 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
+
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
+
+# fixing the ui for the first time the window gets rendered
+first_quote = fetch_quote()
+quote_text = canvas.create_text(150, 207, text=f"{first_quote}", width=250, font=("Arial", 30, "bold"),
                                 fill="white")
 canvas.grid(row=0, column=0)
 
