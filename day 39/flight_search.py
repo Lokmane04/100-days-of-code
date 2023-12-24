3c75324aa0fe833295b89a969389f3a6d9de1978
ReD <baslilokmane04@gmail.com>
2023-12-24 23:55:29 +0100

    finishing the first part of the project (putting data into sheet)

--- day 39/flight_search.py
+++ day 39/flight_search.py
@@ -1,3 +1,13 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    @staticmethod
+    def get_destination_code(city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
--- day 39/main.py
+++ day 39/main.py
@@ -1,7 +1,14 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# 4. Pass the data back to the main.py file, so that you can print the data from main.py
+from data_manager import DataManager
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+print(sheet_data)
+if sheet_data[0]["iataCode"] == "":
+    from flight_search import FlightSearch
+    flight_search = FlightSearch()
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    print(f"sheet_data:\n {sheet_data}")
 
-import requests
-
-response = requests.get(url='https://api.sheety.co/b35a68f81a770a7ae5c6269e80e18086/flightDeals/prices')
-data = response.json()
-print(data)
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
