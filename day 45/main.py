11dc2591812ece9d3ec9bbfbfc00c8f90581fe8a
ReD <baslilokmane04@gmail.com>
2023-12-29 23:55:47 +0100

    getting movies titles from the website

--- day 45/main.py
+++ day 45/main.py
@@ -1,6 +1,15 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://news.ycombinator.com/news")
-soup = BeautifulSoup(response.text, "html.parser")
-print(soup.find(name="a", class_="storylink"))
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
