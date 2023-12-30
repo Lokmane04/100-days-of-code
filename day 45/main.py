be2223c235b9931541038af7d22ef5b20f3996ca
ReD <baslilokmane04@gmail.com>
2023-12-30 04:22:45 +0100

    scraping billboard website (finishing day 45)

--- day 45/main.py
+++ day 45/main.py
@@ -13,3 +13,8 @@ all_movies = soup.find_all(name="h3", class_="title")
 
 movie_titles = [movie.getText() for movie in all_movies]
 movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+
