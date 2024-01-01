0d89b54e134b4d13c5f999b017cebb8e5ac80338
ReD <baslilokmane04@gmail.com>
2024-01-01 23:31:35 +0100

    starter file for the cookie clicker

---
+++ day 48/main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+chrome_driver_path = 'C:/Users/lokmane/OneDrive/Desktop'
+driver = webdriver.Chrome(chrome_driver_path)
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
+
+cookie = driver.find_element_by_id("cookie")
+
+items = driver.find_elements_by_css_selector("#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+timeout = time.time() + 5
+five_min = time.time() + 60 * 5
+
